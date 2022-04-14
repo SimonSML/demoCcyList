006ec8be800a713319e7fc10a67e97caf263eb72
simon.lee <simon.lee@gravitas.com.hk>
2022-04-14 15:29:35 +0800

    add UI test

---
+++ app/src/androidTest/java/demo/currency/myapplication/DemoActivityTest.kt
@@ -0,0 +1,82 @@
+package demo.currency.myapplication
+
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import demo.currency.myapplication.utils.atPosition
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class DemoActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = activityScenarioRule<DemoActivity>()
+
+    @Test
+    fun demoActivityTest() {
+        val loadDataButton = onView(
+            allOf(
+                withId(R.id.btn_load_data), withText("LOAD DATA"),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        loadDataButton.check(matches(isDisplayed())).check(matches(isEnabled()))
+
+        onView(withId(R.id.btn_sort)).check(matches(isDisplayed())).check(matches(not(isEnabled())))
+
+        loadDataButton.perform(click())
+
+        onView(withId(R.id.recyclerview))
+            .check(matches(atPosition(0, hasDescendant(withText("Bitcoin Cash")))))
+
+        onView(withId(R.id.recyclerview))
+            .check(
+                matches(
+                    atPosition(
+                        1, hasDescendant(
+                            allOf(
+                                withId(R.id.tv_name),
+                                withText("Binance Coin")
+                            )
+                        )
+                    )
+                )
+            )
+
+
+        onView(withId(R.id.recyclerview)).perform(
+            actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        onView(withId(R.id.top_bar)).check(matches(withText("Trade")))
+        onView(withId(R.id.input_price)).perform(click(), typeText("12345"))
+        hideKeyboard()
+        onView(withId(R.id.btn_preview)).perform(click())
+        onView(withText("You choose to BUY @$12,345")).check(matches(isDisplayed()))
+
+    }
+
+    private fun hideKeyboard() {
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+    }
+}
---
+++ app/src/androidTest/java/demo/currency/myapplication/utils/TestUtils.kt
@@ -0,0 +1,44 @@
+package demo.currency.myapplication.utils
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.matcher.BoundedMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+
+fun childAtPosition(
+    parentMatcher: Matcher<View>, position: Int
+): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Child at position $position in parent ")
+            parentMatcher.describeTo(description)
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            val parent = view.parent
+            return parent is ViewGroup && parentMatcher.matches(parent)
+                    && view == parent.getChildAt(position)
+        }
+    }
+}
+
+
+fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
+    return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has item at position $position: ")
+            itemMatcher.describeTo(description)
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            val viewHolder = view.findViewHolderForAdapterPosition(position)
+                ?: return false
+            return itemMatcher.matches(viewHolder.itemView)
+        }
+    }
+}
